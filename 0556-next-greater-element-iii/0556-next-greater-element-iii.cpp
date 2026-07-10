47dbac500c214bffb536295fe3a1d5fa7af57f14
Prince Maurya <128956516+Prince200510@users.noreply.github.com>
2026-07-10 22:41:50 +0530

    Time: 0 ms (100%), Space: 7.9 MB (58.29%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s = to_string(n);
+
+        if(next_permutation(s.begin(), s.end())) {
+            long long ans = stoll(s);
+
+            if(ans > INT_MAX) {
+                return -1;
+            } else {
+                return ans;
+            }
+        }
+        return -1;
+    }
+};
